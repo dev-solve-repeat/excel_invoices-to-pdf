d93377bad966acbd84d3c7bf7f2f9e67b813d0ef
Brijesh Babu <brijesh.babutv@gmail.com>
2022-12-18 13:23:42 +0530

    Day25: Loaded the Excel files into dataframes.

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 
+import pandas as pd
+import glob
+
+filepath = glob.glob("invoices/*.xlsx")
+print(filepath)
+
+for path in filepath:
+    df = pd.read_excel(path, sheet_name="Sheet 1")
+    print(df)
