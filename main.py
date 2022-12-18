0995af97f30cbdb288927bef34dc068e35b19c40
Brijesh Babu <brijesh.babutv@gmail.com>
2022-12-18 18:10:08 +0530

    Day26: Added date

--- main.py
+++ main.py
@@ -9,10 +9,17 @@ print(filepath)
 
 for path in filepath:
     df = pd.read_excel(path, sheet_name="Sheet 1")
+
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
+
     filename = Path(path).stem
-    invoice_no = filename.split("-")[0]
+    invoice_no, date = filename.split("-")
+
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Invoice Nr - {invoice_no}")
+    pdf.cell(w=50, h=8, txt=f"Invoice Nr - {invoice_no}", ln=1)
+
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Date - {date}")
+
     pdf.output(f"PDF/{filename}.pdf")
