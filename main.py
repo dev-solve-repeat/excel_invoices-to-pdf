86cc06c0d99b9b7cd570432e1b7266d6e3872210
Brijesh Babu <brijesh.babutv@gmail.com>
2022-12-19 13:28:54 +0530

    Day26: Added table

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ filepath = glob.glob("invoices/*.xlsx")
 print(filepath)
 
 for path in filepath:
-    df = pd.read_excel(path, sheet_name="Sheet 1")
 
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
@@ -20,6 +19,29 @@ for path in filepath:
     pdf.cell(w=50, h=8, txt=f"Invoice Nr - {invoice_no}", ln=1)
 
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Date - {date}")
+    pdf.cell(w=50, h=8, txt=f"Date - {date}", ln=1)
+
+    df = pd.read_excel(path, sheet_name="Sheet 1")
+
+    #Add a header
+    col = df.columns
+    col = [items.replace("_", " ").title() for items in col]
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=10, txt=col[0], border=1)
+    pdf.cell(w=50, h=10, txt=col[1], border=1)
+    pdf.cell(w=40, h=10, txt=col[2], border=1)
+    pdf.cell(w=30, h=10, txt=col[3], border=1)
+    pdf.cell(w=30, h=10, txt=col[4], border=1, ln=1)
+
+    #Add row to the table
+    for index, row in df.iterrows():
+        pdf.set_font(family="Times", size=10)
+        pdf.set_text_color(80, 80, 80)
+        pdf.cell(w=30, h=10, txt=str(row["product_id"]), border=1) #the input to txt should be string, no int
+        pdf.cell(w=50, h=10, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=40, h=10, txt=str(row["amount_purchased"]), border=1)
+        pdf.cell(w=30, h=10, txt=str(row["price_per_unit"]), border=1)
+        pdf.cell(w=30, h=10, txt=str(row["total_price"]), border=1, ln=1)
 
     pdf.output(f"PDF/{filename}.pdf")
